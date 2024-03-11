a075fdf5b37a2fa90594973f9cba6fb4060f93ca
gmuczynski <38670384+GregologyDEV@users.noreply.github.com>
2024-03-11 09:12:48 +0100

    Change 1

--- Zad2/Program.cs
+++ Zad2/Program.cs
@@ -2,6 +2,11 @@
 {
     public static void Main(string[] args)
     {
-        
+        int[] arr = {1, 5, 8, 12, 76, 101};
+
+        foreach (int num in arr)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
