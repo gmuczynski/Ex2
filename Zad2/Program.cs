939ff315ade0f4451490e3b8355d16e7a261e6b2
gmuczynski <162102447+gmuczynski@users.noreply.github.com>
2024-03-18 09:31:37 +0100

    new feature

--- Zad2/Program.cs
+++ Zad2/Program.cs
@@ -1,12 +1,26 @@
-﻿public class Program
+﻿
+
+public class Program
 {
     public static void Main(string[] args)
     {
         int[] arr = {1, 5, 8, 12, 76, 101};
 
-        foreach (int num in arr)
+        Console.WriteLine(Max(arr));
+    }
+
+    public static int Max(int[] num)
+    {
+        int max = 0;
+
+        for(int i = 0; i < num.Length; i++)
         {
-            Console.WriteLine(num);
+            if (num[i] > max)
+            {
+                max = num[i];
+            }
         }
+
+        return max;
     }
 }
